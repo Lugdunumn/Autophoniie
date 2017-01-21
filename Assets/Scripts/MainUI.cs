754abe87eb1ca9f184df418b52d4b5fb0f4d2ebf
solael <yuheng.zhao067@gmail.com>
2017-01-21 02:09:43 +0100

    add interfaces

---
+++ Assets/Scripts/MainUI.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MainUI : MonoBehaviour {
+
+	public Button yesExitButton;
+
+	// Use this for initialization
+	void Start ()
+	{
+		Button exitButton = yesExitButton.GetComponent<Button>();
+		exitButton.onClick.AddListener(doExit);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void doExit()
+	{
+		Application.Quit ();	
+	}
+}
