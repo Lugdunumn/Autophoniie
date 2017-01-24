3a7c9175da93b7d7450367f9a4641136e95bd5cc
solael <yuheng.zhao067@gmail.com>
2017-01-24 03:29:14 +0100

    final commit?

--- Assets/Scripts/MainUI.cs
+++ Assets/Scripts/MainUI.cs
@@ -5,12 +5,17 @@ using UnityEngine.UI;
 public class MainUI : MonoBehaviour {
 
 	public Button yesExitButton;
+	public Button musicToggleButton;
+	public bool musicOff = true;
 
 	// Use this for initialization
 	void Start ()
 	{
 		Button exitButton = yesExitButton.GetComponent<Button>();
 		exitButton.onClick.AddListener(doExit);
+
+		Button musicButton = musicToggleButton.GetComponent<Button> ();
+		musicButton.onClick.AddListener (toggleMusic);
 	}
 	
 	// Update is called once per frame
@@ -21,5 +26,15 @@ public class MainUI : MonoBehaviour {
 	public void doExit()
 	{
 		Application.Quit ();	
+	}
+
+	public void toggleMusic() {
+		if (musicOff == true) {
+			AudioListener.volume = 0.0F;
+			musicOff = false;
+		} else {
+			AudioListener.volume = 0.7F;
+			musicOff = true;
+		}
 	}
 }
